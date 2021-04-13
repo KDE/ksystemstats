b2a05dc160419ec5ae155389097f2901fd85bf74
Arjen Hiemstra <ahiemstra@heimr.nl>
2021-04-13 16:09:24 +0200

    Use ksystemstats_plugins as translation domain
    Since ksysguard_global doesn't make much sense anymore

--- plugins/Messages.sh
+++ plugins/Messages.sh
@@ -3,4 +3,4 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: None
 
-$XGETTEXT $(find -name "*.cpp") -o $podir/ksysguard_plugins_global.pot
+$XGETTEXT $(find -name "*.cpp") -o $podir/ksystemstats_plugins.pot
