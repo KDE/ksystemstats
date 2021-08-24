f09dfae9b777ed62cc1564d4438b4ebad5eb0a7e
Adriaan de Groot <groot@kde.org>
2021-08-24 14:44:07 +0200

    viewer: drop the "rolling" display, expand diagnostics
    When viewing stats, the application waits for updates -- this
    is usually convenient to see how things change over time, but
    for scripting purposes or testing, it can be useful to get just
    one round of output. Do that by default, and introduce `--remain`
    as a flag to get the rolling displays of sensor updates the
    tool produced previously. (Suggested by Arjen)
    
    Since we now want to wait until all the requested values have
    been printed, keep track of which sensors have reported values
    and exit once they all have; this also means we need to check
    for non-existent sensors when running in just-once mode (to
    avoid hanging on typo's). In remain-mode, don't check for typo's:
    let's pretend there might be a way to dynamically add
    sensors and so an initial "typo" could be a way to refer to
    a future sensor.
    
    Additional bits:
    
    - show explicitly when there are no sensors
    - If the user mis-types a sensor name, or data is not
      available for the sensor, then tell the user that
      instead of silently not-reporting-anything.
    - use parser.addOption consistently

--- tools/kstatsviewer/main.cpp
+++ tools/kstatsviewer/main.cpp
@@ -34,13 +34,18 @@ public:
     void showDetails(const QStringList &sensorNames);
 
     void setShowDetails(bool details);
+    void setRemain(bool remain);
 
 private:
     void onNewSensorData(const KSysGuard::SensorDataList &changes);
     void onSensorMetaDataChanged(const KSysGuard::SensorInfoMap &sensors);
     void showSensorDetails(const KSysGuard::SensorInfoMap &sensors);
     KSysGuard::SystemStats::DBusInterface *m_iface;
+    /// Contains the names of all sensors that have **not** reported any data yet
+    QSet<QString> m_unreportedSensors;
     bool m_showDetails = false;
+    /// When @c true, remain connected; otherwise, exit as soon as all subscribed sensors have reported data
+    bool m_remain = false;
 };
 
 int main(int argc, char **argv)
@@ -54,22 +59,22 @@ int main(int argc, char **argv)
     SensorWatcher app(argc, argv);
 
     QCommandLineParser parser;
-    auto listSensorsOption = QCommandLineOption(QStringLiteral("list"), QStringLiteral("List Available Sensors"));
-    parser.addOption(listSensorsOption);
-
+    parser.addOption({ QStringLiteral("list"), QStringLiteral("List Available Sensors") });
     parser.addOption({ QStringLiteral("details"), QStringLiteral("Show detailed information about selected sensors") });
+    parser.addOption({ QStringLiteral("remain"), QStringLiteral("Do not exit, but keep reporting sensor values") });
 
     parser.addPositionalArgument(QStringLiteral("sensorNames"), QStringLiteral("List of sensors to monitor"), QStringLiteral("sensorId1 sensorId2  ..."));
     parser.addHelpOption();
     parser.process(app);
 
-    if (parser.isSet(listSensorsOption)) {
+    if (parser.isSet(QStringLiteral("list"))) {
         app.list();
     } else if (parser.positionalArguments().isEmpty()) {
         qDebug() << "No sensors specified.";
         parser.showHelp(-1);
     } else {
         app.setShowDetails(parser.isSet(QStringLiteral("details")));
+        app.setRemain(parser.isSet(QStringLiteral("remain")));
         app.subscribe(parser.positionalArguments());
         app.exec();
     }
@@ -90,9 +95,38 @@ void SensorWatcher::subscribe(const QStringList &sensorNames)
 {
     m_iface->subscribe(sensorNames);
 
+    m_unreportedSensors = QSet<QString>(sensorNames.begin(), sensorNames.end());
     auto pendingInitialValues = m_iface->sensorData(sensorNames);
     pendingInitialValues.waitForFinished();
     onNewSensorData(pendingInitialValues.value());
+
+    if (!m_remain && !m_unreportedSensors.isEmpty()) {
+        // We want to do one pass only, and there are sensors that
+        // have no initial data. That **might** be because they
+        // are mis-spelled, so go through all the sensors and
+        // check that the names are ok; sensors that do not exist
+        // will be removed (and get a complaining message, too).
+        QSet<QString> existingSensors;
+        // Fill the set of existing sensor names
+        {
+            auto pendingSensors = m_iface->allSensors();
+            pendingSensors.waitForFinished();
+            const auto allSensors = pendingSensors.value();
+            for (auto it = allSensors.constBegin(); it != allSensors.constEnd(); it++) {
+                existingSensors.insert(it.key());
+            }
+        }
+
+        QSet<QString> nonexistentSensors;
+        for (const auto& sensorName : qAsConst(m_unreportedSensors)) {
+            if (!existingSensors.contains(sensorName)) {
+                // This sensor (named on the command-line, and with no data so far) does not exist
+                std::cout << "No sensor named '" << qPrintable(sensorName) << "'\n";
+                nonexistentSensors.insert(sensorName);
+            }
+        }
+        m_unreportedSensors.subtract(nonexistentSensors);
+    }
 
     if (m_showDetails) {
         auto pendingSensors = m_iface->sensors(sensorNames);
@@ -101,12 +135,25 @@ void SensorWatcher::subscribe(const QStringList &sensorNames)
         auto sensors = pendingSensors.value();
         showSensorDetails(sensors);
     }
+
+    if (!m_remain && m_unreportedSensors.isEmpty())
+    {
+        QTimer::singleShot(0, this, &QCoreApplication::quit);
+    }
 }
 
 void SensorWatcher::onNewSensorData(const KSysGuard::SensorDataList &changes)
 {
     for (const auto &entry : changes) {
         std::cout << qPrintable(entry.sensorProperty) << ' ' << qPrintable(entry.payload.toString()) << std::endl;
+        m_unreportedSensors.remove(entry.sensorProperty);
+    }
+    // Use a timer so that we get out of this slot first; since this is **also** called
+    // from subscribe, we want to let that function complete too, just in case the user
+    // has asked for details.
+    if (!m_remain && m_unreportedSensors.isEmpty())
+    {
+        QTimer::singleShot(0, this, &QCoreApplication::quit);
     }
 }
 
@@ -125,6 +172,9 @@ void SensorWatcher::list()
     auto pendingSensors = m_iface->allSensors();
     pendingSensors.waitForFinished();
     auto sensors = pendingSensors.value();
+    if (sensors.count() < 1) {
+        std::cout << "No sensors available.";
+    }
     for (auto it = sensors.constBegin(); it != sensors.constEnd(); it++) {
         std::cout << qPrintable(it.key()) << ' ' << qPrintable(it.value().name) << std::endl;
     }
@@ -133,6 +183,11 @@ void SensorWatcher::list()
 void SensorWatcher::setShowDetails(bool details)
 {
     m_showDetails = details;
+}
+
+void SensorWatcher::setRemain(bool remain)
+{
+    m_remain = remain;
 }
 
 void SensorWatcher::showSensorDetails(const KSysGuard::SensorInfoMap &sensors)
