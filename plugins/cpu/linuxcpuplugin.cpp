KDE/ksystemstats

commit 478e766d19b333a7cc2316ed11e002928673ba2c

plugins/cpu: Ignore invalid CPU info when reading /proc/cpuinfo
On some systems, /proc/cpuinfo contains non-core related data. When that
happens, we would end up creating an invalid CpuInfo object that would
later on crash ksystemstats. Instead, ignore said CpuInfo object
properly so we don't crash.

Patch based on Jonathan L Hanmann's suggestion from the bug report.

BUG: 493093


(cherry picked from commit c5bc0067b31f6c2bdee127702c6cc4717c770808)

Co-authored-by: Arjen Hiemstra <ahiemstra@heimr.nl>

plugins/cpu/linuxcpuplugin.cpp
@@ -80,7 +80,9 @@ LinuxCpuPluginPrivate::LinuxCpuPluginPrivate(CpuPlugin *q)
             const int delim = line.indexOf(":");
             const QByteArray field = line.left(delim).trimmed();
             const QByteArray value = line.mid(delim + 1).trimmed();
-            if (field == "processor") {
+            if (field == "Serial") {
+                break;
+            } else if (field == "processor") {
                 info.id = value.toInt();
             } else if (field == "physical id") {
                 info.cpu = value.toInt();
@@ -93,8 +95,13 @@ LinuxCpuPluginPrivate::LinuxCpuPluginPrivate(CpuPlugin *q)
             }
         }
 
-        cpus.push_back(info);
-        cpuCount = std::max(cpuCount, info.cpu);
+        // If we don't have a valid ID, consider the entire CPU info to be invalid.
+        // This mainly happens when there is additional non-core related data in
+        // /proc/cpuinfo.
+        if (info.cpu != -1) {
+            cpus.push_back(info);
+            cpuCount = std::max(cpuCount, info.cpu);
+        }
     }
 
     // cpuCount is based on the indices of the cpus, which means it is off by

commit 2f0903224c21e9e1107063d3ae4adabed25f555b

plugins/cpu: Test for the proper CPU property, skip nonexistent CPUs
Commit 478e766d19b3 regressed ksystemstats on many ARM systems, since it
mistakenly tests the "cpu" property of the CPU object (which is is the
"physical id") and not the "id" property (which is the actual CPU
number). For systems without a "physical id", this ignores all CPUs and
ends up crashing on a NULL deref later when it tries to look up CPU
numbers from /proc/stat.

Fix the incorrect check and add an extra NULL guard, so if we fail to
parse /proc/cpuinfo properly we don't crash. We only need an "id"
property to consider the CPU object valid, and that property is required
to identify the CPU in the first place, so it is the only one that needs
to be tested.

Also guard insertion into m_cpusBySystemIds if the keys do not exist
(this is what wants `cpu` and `core`). This isn't strictly necessary,
but more correct. Consumers of that already test for entries correctly
and don't segfault if missing.

BUG: 495524
Fixes: 478e766d19b3 ("plugins/cpu: Ignore invalid CPU info when reading /proc/cpuinfo")

plugins/cpu/linuxcpuplugin.cpp
@@ -98,9 +98,9 @@ LinuxCpuPluginPrivate::LinuxCpuPluginPrivate(CpuPlugin *q)
         // If we don't have a valid ID, consider the entire CPU info to be invalid.
         // This mainly happens when there is additional non-core related data in
         // /proc/cpuinfo.
-        if (info.cpu != -1) {
+        if (info.id != -1) {
             cpus.push_back(info);
-            cpuCount = std::max(cpuCount, info.cpu);
+            cpuCount = std::max(cpuCount, info.id);
         }
     }
 
@@ -114,7 +114,9 @@ LinuxCpuPluginPrivate::LinuxCpuPluginPrivate(CpuPlugin *q)
     for (const auto &entry : std::as_const(cpus)) {
         auto cpu = new LinuxCpuObject(QStringLiteral("cpu%1").arg(entry.id), names.value(entry.id), entry.frequency, m_container);
         m_cpus.insert(entry.id, cpu);
-        m_cpusBySystemIds.insert({entry.cpu, entry.core}, cpu);
+        if (entry.cpu != -1 && entry.core != -1) {
+            m_cpusBySystemIds.insert({entry.cpu, entry.core}, cpu);
+        }
     }
 
     addSensors();
@@ -160,7 +162,11 @@ void LinuxCpuPluginPrivate::update()
             m_allCpus->update(system + irq + softirq, user + nice , iowait + steal, idle);
         } else if (line.startsWith("cpu")) {
             auto cpu = m_cpus.value(std::atoi(line.mid(strlen("cpu"))));
-            cpu->update(system + irq + softirq, user + nice , iowait + steal, idle);
+            if (cpu) {
+                cpu->update(system + irq + softirq, user + nice , iowait + steal, idle);
+            } else {
+                qWarning() << "Unknown CPU" << line;
+            }
         }
     }
 }